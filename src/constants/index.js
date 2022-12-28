18da82c47d870170eaae8c008e5626831c08ac7e
Guilherme Moura <mouraggui@gmail.com>
2022-12-28 12:41:13 -0300

    feat: consts folder

---
+++ src/constants/index.js
@@ -0,0 +1,11 @@
+export const CURRENCIES = [
+  { code: 'USD', title: 'United States Dollar' },
+  { code: 'EUR', title: 'Euro' },
+  { code: 'BRL', title: 'Brazilian Real' },
+  { code: 'CAD', title: 'Canadian Dollar' },
+  { code: 'GBP', title: 'Pound Sterling' },
+];
+
+export const COLORS = {
+  BADGE_BACKGROUND_COLOR: '#186200',
+};
