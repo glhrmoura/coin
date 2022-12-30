4431b6e516a9b0e8a2eb524d4645fcaf242eaba2
Guilherme Moura <mouraggui@gmail.com>
2022-12-30 17:25:59 -0300

    fix: importing style base file

--- src/extension/popup/index.js
+++ src/extension/popup/index.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 
+import './assets/css/base.css';
+
 import PopupApp from './index.vue';
 
-createApp(PopupApp)
-  .mount('#app');
+createApp(PopupApp).mount('#app');
