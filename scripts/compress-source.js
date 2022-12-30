db5a0e602b4281acd9cd0770125a750d3944f1e5
Guilherme Moura <mouraggui@gmail.com>
2022-12-29 21:45:40 -0300

    fix: remove scripts folder

--- scripts/compress-source.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-
-const AdmZip = require('adm-zip');
-
-const zip = new AdmZip();
-
-zip.addLocalFolder('./src');
-zip.writeZip(path.resolve(__dirname, '../dist/coins.zip'));
